c384315ddcf12466991ec3fa9d46b32f550ac3d9
paul <paul@pauls-air.attlocal.net>
2022-04-23 14:30:44 -0500

    added a print statement on line 11 in memorycardgame.java

--- src/MemoryCardGame/MemoryCardGame.java
+++ src/MemoryCardGame/MemoryCardGame.java
@@ -8,6 +8,7 @@ public class MemoryCardGame {
 		if (args.length == 1) {
 			if (args[0].equals("-server")) {
 				System.out.println("not implemented yet");
+				System.out.println("Hello");
 			} else if (args[0].equals("-client")) {
 				ClientGUI client = new ClientGUI();
 				client.setVisible(true);
